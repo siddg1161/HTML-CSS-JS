46c3e96e16e370e0b5757c6319528cdc6bea62c6
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-03-09 00:17:30 +0530

    Add initial HTML structure with CSS styles

---
+++ Day-09-HTML & CSS/index.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="babbar.css">
+    <!--Internal Css-->
+    <style>
+        <!--elementselector-->
+        h1{ 
+            color: greenyellow;
+        }
+        <!--classselector-->
+        .paraclass
+        {
+            color:blue;
+        }
+        <!--idselector-->
+        #paraid
+        {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        <!--groupselector-->
+        p,article{
+            width: 500px;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Welcome to my page</h1>
+    <!--Inline Css-->
+    <p style="background-color: blueviolet;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti, aut saepe doloremque in a facere laborum provident earum debitis.</p>
+    <p class="paraclass">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iusto, provident blanditiis, exercitationem odit architecto consectetur nisi libero voluptate voluptatibus temporibus enim neque. Optio voluptatibus maxime, asperiores voluptatem consequatur nulla!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, saepe.</p>
+    <p id="paraid"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi quo esse error amet tempore ex dolorem, velit sed dignissimos quisquam libero nobis minima sit earum expedita necessitatibus praesentium modi nisi? Commodi eveniet assumenda laborum quas nostrum facilis. Nisi eos eaque corrupti vero, cumque culpa quasi molestiae praesentium adipisci exercitationem.</p>
+    <article><h2>Hello Hi</h2></article>
+</body>
+</html>
