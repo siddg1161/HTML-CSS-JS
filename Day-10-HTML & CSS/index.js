80cddd0570d02509f2c0e860233be1071f8d748c
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-03-10 00:55:27 +0530

    Create index.js

---
+++ Day-10-HTML & CSS/index.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p{
+            font-style: italic;
+            font-size: 20px;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+        }
+        #para1{
+            color: blue;
+        }
+        #para2{
+            background-color: rgb(0, 30, 255)
+        }
+        .class3{
+            background-color: #ffabff;
+        }
+        .class2
+        {
+            background-color: hsl(120, 50%,80%);
+        }
+    </style>
+</head>
+<body>
+    <h2>Welcome to Code help</h2>
+    <p id="para1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sed nemo ab dolorem consectetur, labore provident expedita sit omnis ipsa.</p>
+    <p id="para2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, libero officia aperiam sed rem eos eligendi minus eius molestiae, maiores adipisci ducimus error aliquid est laudantium voluptatibus repellat ratione molestias!
+
+    </p>
+    <p id="para3" class="class3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, natus consequuntur consequatur adipisci assumenda soluta itaque, et praesentium tempora ab impedit facilis similique placeat cupiditate? Ab omnis sapiente qui repellendus suscipit hic eligendi vitae atque corrupti. Modi voluptates dolores non aperiam, ullam ea adipisci unde vel doloremque odio at ut.
+
+    </p>
+    <p class="class2">Lorem, ipsum dolor.</p>
+</body>
+</html>
