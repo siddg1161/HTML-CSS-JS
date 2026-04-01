d52076a57f67ff894678b7a28b2a0be0be6d64d2
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-04-02 00:25:53 +0530

    Add JavaScript code for various operations

---
+++ Day-32 HTML CSS JS/script.js
@@ -0,0 +1,38 @@
+let a=10;
+let b=5;
+
+console.log(a%5);
+console.log(a**b);
+
+a+=2;
+console.log(a);
+
+console.log(10<5);
+
+console.log('5'==5);
+console.log('5'===5);
+
+let age=20;
+let status1=(age>18)? "can vote" : "cannot vote";
+console.log(status1);                                           
+
+let ans=(true && true && false);
+console.log(ans);
+
+console.log(false || 'babbar');
+console.log(false || 7 || 10 || 20); //short-cricuting
+
+
+console.log(10&5);
+
+console.log(~(0));
+
+let Age=5;
+if(Age>18)
+{
+    console.log("can vote");
+}
+else
+{
+    console.log("not vote");
+}
