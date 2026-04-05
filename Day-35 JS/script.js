72b57cd05eac92c9d4b406db5677fa4067aae0c0
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-04-06 01:56:15 +0530

    Create script.js

---
+++ Day-35 JS/script.js
@@ -0,0 +1,49 @@
+for(let i=1;i<=10;i++)
+{
+  console.log("Hi");
+}
+
+
+let j=1;
+while(j<=4)
+{
+    console.log("Bye");
+    j++;
+}
+
+let k=1;
+do{
+   console.log("20");
+   k++;
+}
+while(k<=7);
+
+let name="babbar haj";
+let name1='siddhart bye ';
+let name2=`sid
+how
+sid
+forever`;
+let name3=new String("siddharth gupta");
+
+console.log(name);
+console.log(name1);
+console.log(name2);
+console.log(name3);
+let op1='English';
+let op2='Hindi';
+let ans=op1+op2;
+console.log(ans);
+
+let finalAns=`${op1}${op2}`;
+console.log(finalAns);
+
+console.log(op2.length);
+
+console.log(op1.toUpperCase());
+console.log(op2.toLowerCase());
+
+let sentence=" Hello jee kaise ho saare";
+let words=sentence.split(' ');
+console.log(words);
+console.log(words.join('-'));
