72c0ffaee87f15edc9f43ea2448e0afb40222282
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-04-14 01:17:28 +0530

    Add script.js with various Math and Date methods

---
+++ Day-40 JS/script.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+console.log(Math.max(20,30,10));
+console.log(Math.round(1.5));
+console.log(Math.floor(1.9));
+console.log(Math.ceil(1.2));
+console.log(Math.abs(-5));
+console.log(Math.random());
+console.log(Math.sqrt(4));
+console.log(Math.pow(2,10));
+
+
+let curr=new Date();
+console.log(curr);
+
+let  date=new Date('June 20 1998 7:15');
+console.log(date);
+
+let newDate=new Date(1998,5,20,7);
+console.log(newDate);
