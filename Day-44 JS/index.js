0384b15a41b3b9d4e29bd269f2e7a8853647ba4e
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-04-18 01:33:45 +0530

    Create index.js

---
+++ Day-44 JS/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="fdiv" style="background-color: aqua; padding: 2rem;">
+
+        first div
+
+        <p id="fpara" class="firstClass secondClass" style="background-color: rgb(34, 223, 34); padding: 1rem;">first para</p>
+        
+        <div id="sdiv" style="padding: 1.2rem; background-color: yellow;">
+            second div
+
+            <p id="spara" style="background-color: red; padding: 0.8rem;">second para</p>
+        
+        </div>
+    
+    </div>
+
+</body>
+</html>
